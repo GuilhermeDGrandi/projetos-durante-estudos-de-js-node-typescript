e02238fe7b6b582a95e6f3896ac7bbf20d2c2d98
GuilhermeDGrandi <guilhermedgrandi@gmail.com>
2025-01-06 17:41:57 -0300

    aprendendo nodemon

---
+++ node/aula7/server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send(`
+        <form action="/" method="POST">
+        Nome:<input type="text" name="nome">
+        <button>send</button>
+        </form>         
+        `);
+});
+app.post('/' , (req,res)=>{
+    res.send('recebi o formulário')
+})
+
+
+app.get('/contato', (req, res) => {
+    res.send()
+})
+
+
+app.listen(3000 ,() =>{
+    console.log('Acessar http://localhost:3000')
+    
+    console.log('servidor executando porta 3000')
+});
+
+

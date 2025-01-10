80cad1f2c3a46a0aa3ce2ed85a8a5cc71dbb6ec1
GuilhermeDGrandi <guilhermedgrandi@gmail.com>
2025-01-10 18:34:03 -0300

    Encerrado capitulo de node+Express, iniciar agora um projeto com o aprendizado

--- node/aula11MongoDB/server.js
+++ node/aula11MongoDB/server.js
@@ -2,33 +2,53 @@
 //req.query vem dps do '?' na url
 //req.body vem do post do formulário
 
-
+//importa coisas relacionadas ao meu ambiente de desenvolvimento(senhas, connectstring, etc)
+require('dotenv').config()
 
 const express = require('express')
 const app = express()
+//modela base de dados
 const mongoose = require('mongoose')
-
-
-require('dotenv').config()
 
 mongoose.connect(process.env.CONNECTIONSTRING)
 .then(()=>{
-    console.log('conectado!')
     app.emit('pronto')})
+    .catch(e => console.log(e))
 
+//session é para identificar o navegador do cliente(cookies)
 const session = require('express-session')
+
+//sessoes vão ser salvas na base de dados
 const MongoStore = require('connect-mongo');
+
+//mensagens auto destrutivas(mensagens de erro e etc)
 const flash = require('connect-flash')
 
+//rotas da aplicação(/home, /contato ,etc)
 const routes = require('./routes')
+
+//trabalhar com caminhos
 const path = require('path')
-const {middlewareGlobal, outroMiddlewareGlobal} = require('./src/middlewares/middleware')
+
+//recomendação do express, segurança
+const helmet = require('helmet')
+
+//token para verificação de formulários, para segurança
+const csrf = require('csurf')
+
+//middlewares, funçoes executadas durante a rota (funçoes executadas no meio)
+const {middlewareGlobal, checkCsrfError, csrfMiddleware} = require('./src/middlewares/middleware')
+
+//
+app.use(helmet())
 
 app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 
+//arquivos que são estáticos e podem ser acessados diretamente
 app.use(express.static('./public'))
 
-
+//configuração de seção
 const sessionOption = session({
     secret: 'adoawbdawdawdaadaw',
     store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
@@ -42,14 +62,21 @@ const sessionOption = session({
 app.use(sessionOption)
 app.use(flash())
 
+//telas
 app.set('views', './src/views')
 app.set('view engine', 'ejs')
 
+app.use(csrf())
 //nossos próprios middlewares
 app.use(middlewareGlobal)
+app.use(checkCsrfError)
+app.use(csrfMiddleware)
 //app.use(outroMiddlewareGlobal)
+
+//usando as rotas
 app.use(routes)
 
+//aplicação escutando
 app.on('pronto', ()=>{
     app.listen(3000 ,() =>{
         console.log('Acessar http://localhost:3000')
--- node/aula11MongoDB/src/middlewares/middleware.js
+++ node/aula11MongoDB/src/middlewares/middleware.js
@@ -7,3 +7,13 @@ exports.outroMiddlewareGlobal = (req, res, next)=> {
     next();
 }
 
+exports.checkCsrfError = (error, req, res, next) =>{
+    if(error && error.code === 'EBADCSRFTOKEN'){
+        return res.render('404')
+    }
+}
+
+exports.csrfMiddleware = (req, res, next)=>{
+    res.locals.csrfToken= req.csrfToken()
+    next();
+}

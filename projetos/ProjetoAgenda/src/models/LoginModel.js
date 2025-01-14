028d54ae8e183be7b857b1aed747458a436ac8b8
GuilhermeDGrandi <guilhermedgrandi@gmail.com>
2025-01-14 20:02:49 -0300

    criando sessão do usuário, e validação de login e senha

--- projetos/ProjetoAgenda/src/models/LoginModel.js
+++ projetos/ProjetoAgenda/src/models/LoginModel.js
@@ -17,6 +17,29 @@ class Login {
     this.user=null
   }
 
+  async login(){
+    this.valida()
+    if(this.errors.length > 0) return
+
+    this.user = await LoginModel.findOne({email:this.body.email})
+
+    if(!this.user){
+       this.errors.push('Usuário não existe')
+      	return
+    }
+
+    if(!bcryptjs.compareSync(this.body.password, this.user.password)){
+      this.errors.push('Senha inválida')
+      this.user=null
+      return
+    }
+
+     
+
+    
+
+  }
+
   async register(){
     this.valida()
 
@@ -29,8 +52,8 @@ class Login {
 
   }
   async userExists(){
-    const user = await LoginModel.findOne({email:this.body.email})
-    if(user){
+    this.user = await LoginModel.findOne({email:this.body.email})
+    if(this.user){
       this.errors.push('Usuário já existe')
     }
   }

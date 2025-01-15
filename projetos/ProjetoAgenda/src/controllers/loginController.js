b698261276616447ca39f0d9105c72d191e3a4a5
GuilhermeDGrandi <guilhermedgrandi@gmail.com>
2025-01-15 19:29:00 -0300

    Projeto feito juntamente com um curso onde visualizei diversos "erros", proximo commit será a correção por conta própria

--- projetos/ProjetoAgenda/src/controllers/loginController.js
+++ projetos/ProjetoAgenda/src/controllers/loginController.js
@@ -1,33 +1,32 @@
-const Login = require('../models/LoginModel')
+const Login = require('../models/LoginModel');
 
-exports.index = (req,res) =>{
-  if(req.session.user) return res.render('login-logado')
-    return res.render('login')
-}
+exports.index = (req, res) => {
+  if(req.session.user) return res.render('login-logado');
+  return res.render('login');
+};
 
 exports.register = async function(req, res) {
-    try {
-      const login = new Login(req.body);
-      await login.register();
-  
-      if(login.errors.length > 0) {
-        req.flash('errors', login.errors);
-        req.session.save(function() {
-          return res.redirect('/login/index');
-        });
-        return;
-      }
+  try {
+    const login = new Login(req.body);
+    await login.register();
 
-      req.flash('success', 'Seu Usuário foi criado com sucesso');
+    if(login.errors.length > 0) {
+      req.flash('errors', login.errors);
       req.session.save(function() {
-        return res.redirect('/login/index');
+        return res.redirect('back');
       });
-
-        }catch(e){
-        console.log(e)
-        return res.render('404')
+      return;
     }
-}
+
+    req.flash('success', 'Seu usuário foi criado com sucesso.');
+    req.session.save(function() {
+      return res.redirect('back');
+    });
+  } catch(e) {
+    console.log(e);
+    return res.render('404');
+  }
+};
 
 exports.login = async function(req, res) {
   try {
@@ -37,25 +36,23 @@ exports.login = async function(req, res) {
     if(login.errors.length > 0) {
       req.flash('errors', login.errors);
       req.session.save(function() {
-        return res.redirect('/login/index');
+        return res.redirect('back');
       });
       return;
     }
 
-    req.flash('success', 'Login efetuado com sucesso!');
-    req.session.user = login.user
+    req.flash('success', 'Você entrou no sistema.');
+    req.session.user = login.user;
     req.session.save(function() {
-      return res.redirect('/login/index');
+      return res.redirect('back');
     });
-
-      }catch(e){
-      console.log(e)
-      return res.render('404')
+  } catch(e) {
+    console.log(e);
+    return res.render('404');
   }
-}
+};
 
-exports.logout = function(req, res){
+exports.logout = function(req, res) {
   req.session.destroy();
-  res.redirect('/')
-}
-
+  res.redirect('/');
+};
--- projetos/ProjetoAgenda/src/models/HomeModel.js
+++ projetos/ProjetoAgenda/src/models/HomeModel.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 const HomeSchema = new mongoose.Schema({
   titulo: { type: String, required: true },
-  descricao: { type: String, required: true }
-  
+  descricao: String
 });
 
 const HomeModel = mongoose.model('Home', HomeSchema);

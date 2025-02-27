feeee10db24f8b01484d075856b229bde320d641
guilherme <guilhermedgrandi@gmail.com>
2025-02-27 17:11:20 -0300

    Criado user no banco com bcrypt na senha

--- API/src/controllers/HomeController.js
+++ API/src/controllers/HomeController.js
@@ -1,8 +1,18 @@
+import Aluno from '../models/Aluno'
+
+
 class HomeController{
-  index(req,res){
-    res.json({
-      tudoCerto: true
+
+  async index(req,res){
+    const novoAluno = await Aluno.create({
+      nome: 'Guilherme',
+      sobrenome: 'De Grandi',
+      email: 'teste@teste.com',
+      idade: '21',
+      peso: '60',
+      altura: '160',
     })
+    res.json(novoAluno)
   }
 }
 

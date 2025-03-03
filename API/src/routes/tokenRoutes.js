ea13caad1679e2d1cb648e4af642e0824c804dcb
guilherme <guilhermedgrandi@gmail.com>
2025-03-03 19:15:07 -0300

    Terminado parte de usuário com validação de Token

---
+++ API/src/routes/tokenRoutes.js
@@ -0,0 +1,12 @@
+import { Router } from 'express'
+import tokenController from '../controllers/TokenController'
+
+
+const router = new Router()
+
+router.post('/', tokenController.create)
+
+export default router;
+
+
+
--- API/src/routes/userRoutes.js
+++ API/src/routes/userRoutes.js
@@ -1,10 +1,18 @@
 import { Router } from 'express'
 import userController from '../controllers/UserController'
+import loginRequired from '../middlewares/loginRequired'
 
 
 const router = new Router()
 
-router.post('/', userController.create)
+
+//router.get('/',loginRequired, userController.index) // lista usuários
+//router.get('/:id', userController.show) //lista usuário
+
+
+router.post('/', userController.create) // criar usuário
+router.put('/', loginRequired, userController.update) 
+router.delete('/', loginRequired, userController.delete)
 
 export default router;
 
